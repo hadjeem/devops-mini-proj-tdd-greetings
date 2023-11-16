6982fe5a9acc4a27c3a4325191cbbb056d750a63
Semih <semih.turkoglu@student.vinci.be>
2023-11-16 16:00:39 +0100

    test: greet with Amy, BRIAN and Charlotte

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 function greet (name) {
   if (name === '' || name === null || name === undefined) return 'Hello, my friend.'
+
+  if (typeof (name) === 'object') {
+    for (const nameP of name) {
+      if (nameP === nameP.toUpperCase()) {
+        return 'Hello, Amy and Charlotte. AND HELLO BRIAN !'
+      }
+    }
+  }
+
   if (typeof (name) === 'object' && name.length >= 3) {
     return moreThanTwoParameters(name)
   }
+
   if (typeof (name) === 'object') return `Hello, ${(name[0])} and ${(name[1])}.`
   if (name.toUpperCase() === name) return `HELLO, ${(name)}!`
   return `Hello, ${(name)}.`
