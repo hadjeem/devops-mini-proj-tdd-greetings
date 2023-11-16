c88a1748838b8d7bd7ce735d5e7ae76e25c71cc5
dimitri-danloy-vinci <129385593+dimitri-danloy-vinci@users.noreply.github.com>
2023-11-16 15:24:47 +0100

    refactor: greet with more than 2 parameter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 function greet (name) {
   if (name === '' || name === null || name === undefined) return 'Hello, my friend.'
-  if (typeof (name) === 'object' && name.length === 3) return 'Hello, Amy, Brian and Charlotte.'
+  if (typeof (name) === 'object' && name.length >= 3) {
+    let result = 'Hello'
+    for (let index = 0; index < name.length - 1; index++) {
+      result += `, ${(name[index])}`
+    }
+    result += ` and ${(name[name.length - 1])}.`
+    return result
+  }
   if (typeof (name) === 'object') return `Hello, ${(name[0])} and ${(name[1])}.`
   if (name.toUpperCase() === name) return `HELLO, ${(name)}!`
   return `Hello, ${(name)}.`
